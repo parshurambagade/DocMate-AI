9428d38178d3bc0190e8b23d2e6c63d3986430a8
parshurambagade <parshurambagade855@gmail.com>
2026-04-18 12:44:29 +0530

    feat: add guest mode chat id generation logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+"use client"
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 
 export default function Page() {
+  const [chatId, setChatId] = useState(localStorage.getItem("chatId"))
+
+  useEffect(() => {
+    if (!chatId) {
+      const newChatId = uuidv4()
+      localStorage.setItem("chatId", newChatId)
+      setTimeout(() => {
+        setChatId(newChatId)
+      }, 0)
+    }
+  }, [])
+  
+  console.log(chatId)
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
+          <p>{chatId}</p>
           <Button className="mt-2">Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
